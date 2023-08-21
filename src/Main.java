699c30e982af51007772f898286c1901f8be8774
khilola.kushbakova <xilola_kushbakova@mai.ru>
2023-08-21 23:43:30 +0300

    Hw is done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Author author1 = new Author("Dan Brown");
+        Book book1 = new Book(" The Da Vinci Code ", author1, 2003);
+
+
+
+        Author author2 = new Author("Daniel Keyes");
+        Book book2 = new Book("The Minds of Billy Milligan", author2,1995);
+
+
+        System.out.println( book1.toString());
+
+        System.out.println( book2.toString());
+
+
     }
 }
